77034d3903e7c525678eab79dbc18a33a34efdce
ali <aliaqel2021@gmail.com>
2022-03-20 23:24:29 +0300

    added SpringBootApplication and finished.

---
+++ src/main/java/ru/itis/EntityApplication.java
@@ -0,0 +1,13 @@
+package ru.itis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntityApplication.class, args);
+    }
+
+}
